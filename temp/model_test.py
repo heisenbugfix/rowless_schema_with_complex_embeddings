38407c80e32f8b6795bc54dc7ec2cb03f56bfb6c
itsmilishah <13bce105@nirmauni.ac.in>
2017-12-04 13:34:42 -0500

    Minor changes

--- temp/model_test.py
+++ temp/model_test.py
@@ -1,7 +1,9 @@
+import sys
+sys.path.insert(0,'../code')
 import tensorflow as tf
 import numpy as np
 import random
-from code.model import RowlessModel
+from model import *
 """
 d1,d2,d3 = type_1[0][0],type_1[0][1],type_1[0][2]
 l1,l2,l3 = np.reshape(type_1[1][0],(-1)),np.reshape(type_1[1][1],(-1)),np.reshape(type_1[1][2],(-1))
@@ -28,34 +30,7 @@ for i in range(self.n_epochs):
         self.sess.run(self.train_step_1,feed_dict={self.input_1:d1,self.input_2:d2,self.input_3:d3,\
                                                self.seq_len1:l1,self.seq_len2:l2,self.seq_len3:l3})
 """
-
-# ## Preprocessing functions ##
-# def preprocess_file(f):
-#     """Returns np.array [9 x n_sents]. Columns : [e1,e1_str,e1_start_idx,e1_end_idx,e2,e2_str,e2_start_idx,e2_end_idx,sent]"""
-#     with open(f,'rb') as f:
-#         test_lines = [(str(codecs.unicode_escape_decode(str(i)[2:-3])[0])[1:].split('\t')) for i in f.readlines()]
-#     test_lines = np.array([i for i in test_lines if len(i)==13])
-#     ip = test_lines.T
-#     ip[12] = np.array([re.sub('[^\w\s]','',i.lower()) for i in ip[12]])
-#     return ip[[0,2,3,4,5,7,8,9,12]]
-
-# # verify whether the variables are reused
-# for v in tf.global_variables():
-#    print(v.name)
-#
-# # concat the three outputs
-# output = tf.concat...
-#
-# # Pass it to the final_lstm layer and out the logits
-# logits = final_layer(output, ...)
-#
-# train_op = ...
-#
-# # train
-# sess.run(train_op, feed_dict{input_1: in1, input_2: in2, input_3:in3, labels: ...}
-
-
-r = RowlessModel(12, 20, [2, 3, 4], [4, 6, 7], [1, 2, 3], True, 10, 20)
+r = RowlessModel(12, 20, True, 10, 20)
 # type_1 : 3 x batch_size x wordvec_dim
 batch_size = 10
 wordvec_dim = 12
@@ -69,7 +44,5 @@ d1_ = np.copy(d1)
 d2_ = np.copy(d2)
 d3_ = np.copy(d3)
 print(r.sess.run(r.loss_sentence,feed_dict={r.input_1:d1_,r.input_2:d2_,r.input_3:d3_,r.seq_len1:l1,r.seq_len2:l1,r.seq_len3:l3}))
-r.train(type_1)
-print(r.sess.run(r.loss_sentence,feed_dict={r.input_1:d1,r.input_2:d2,r.input_3:d3}))
 print(tf.__version__)
 print("OK")
