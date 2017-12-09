a0b5bf39d9cc4fc089d78e41f407e1d52071f433
Mili Shah <13bce105@nirmauni.ac.in>
2017-12-09 14:31:16 -0500

    Add funtion to create entity pairs vocab

--- code/prep_ip.py
+++ code/prep_ip.py
@@ -12,7 +12,7 @@ def preprocess_file(f):
     #ip[:,12] = np.array([re.sub('[^\w\s]','',i.lower()) for i in ip[:,12]])
     return ip[:,[0,2,3,4,5,7,8,9,12]]
 
-def create_entity_pairs_dict(preprocessed_sents,e1_idx=0,e2_idx=4):
+def create_entity_pairs_dict_and_table(preprocessed_sents,e1_idx=0,e2_idx=4):
     """ 
     Creates a dict for all the sentences with the same entity pairs.
     Key : <e1,e2>
@@ -20,9 +20,12 @@ def create_entity_pairs_dict(preprocessed_sents,e1_idx=0,e2_idx=4):
     """
     d = dict()
     for s in preprocessed_sents:
-        key = (s[0],s[4])
+        key = (s[e1_idx],s[e2_idx])
         if key in d:
             d[key] = np.append(d[key],s)
         else:
             d[key] = np.array([s])
     return d
+
+def create_entity_pairs_vocab(preprocessed_sents,e1_idx=0,e2_idx=4):
+    return {tuple(i) for i in preprocessed_sents[:,[e1_idx,e2_idx]]}
