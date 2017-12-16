303bf47f7249690f6f60e09a19ab5dcdaf9f00cf
ashish <ashish.ranjan2000@gmail.com>
2017-12-16 01:13:41 -0500

    code cleaned. removed kb_embedding boolean flag

--- code/model.py
+++ code/model.py
@@ -4,23 +4,19 @@ import tensorflow as tf
 # TODO: Make model more generic. Need to abstractions
 class RowlessModel(object):
     # Create input placeholder for the network
-    def __init__(self, wordvecdim, num_units, kb_relation_use=False,
-                 vocab_size=None, embedding_size=None, mode="train", emb_type="real"):
+    def __init__(self, wordvecdim, num_units, vocab_size=None, embedding_size=None, mode="train", emb_type="real"):
 
-        # For training KB relation embeddings
-        if kb_relation_use:
-            assert embedding_size == num_units
-            assert vocab_size is not None
-            self.vocab_size = vocab_size
-            if embedding_size is None:
-                self.embedding_size = 50
-            else:
-                self.embedding_size = embedding_size
+        assert embedding_size == num_units
+        assert vocab_size is not None
+        self.vocab_size = vocab_size
+        if embedding_size is None:
+            self.embedding_size = 50
+        else:
+            self.embedding_size = embedding_size
 
         self.emb_type = emb_type
-        self.kb_relation_use = kb_relation_use
         self.create_conditional_placeholders()
-        self.create_placeholders_kb(kb_relation_use)
+        self.create_placeholders_kb()
         self.create_kb_outputs()
         self.wordvec_dim = wordvecdim
         self.num_units = num_units
@@ -40,12 +36,11 @@ class RowlessModel(object):
 
     # Create KB embeddings
     def create_kb_embeddings(self, rel_ids):
-        if self.kb_relation_use:
-            self.rel_embeddings = tf.get_variable("relation_embeddings",
-                                                  [self.vocab_size, self.embedding_size], dtype=tf.float32)
-            emb_rel_ids = tf.nn.embedding_lookup(self.rel_embeddings, rel_ids)
-            emb_rel_ids = tf.reshape(emb_rel_ids, shape=[-1, emb_rel_ids.shape[2]])
-            return emb_rel_ids
+        self.rel_embeddings = tf.get_variable("relation_embeddings",
+                                              [self.vocab_size, self.embedding_size], dtype=tf.float32)
+        emb_rel_ids = tf.nn.embedding_lookup(self.rel_embeddings, rel_ids)
+        emb_rel_ids = tf.reshape(emb_rel_ids, shape=[-1, emb_rel_ids.shape[2]])
+        return emb_rel_ids
 
     # Creating conditional placeholders for switching inputs between LSTM and KB Embeddings
     def create_conditional_placeholders(self):
@@ -78,10 +73,9 @@ class RowlessModel(object):
         self.seq_len3 = tf.placeholder(tf.int32, [None])
 
     # Placeholders for relation as input data
-    def create_placeholders_kb(self, kb_relation_use=False):
-        if kb_relation_use:
-            self.input_2_r2 = tf.placeholder(tf.int32, [None, 1], name="r2")
-            self.input_3_r3 = tf.placeholder(tf.int32, [None, 1], name="r3")
+    def create_placeholders_kb(self):
+        self.input_2_r2 = tf.placeholder(tf.int32, [None, 1], name="r2")
+        self.input_3_r3 = tf.placeholder(tf.int32, [None, 1], name="r3")
 
     # create a shared rnn layer
     def lstm_share(self, num_units, input, seq_len):
@@ -114,7 +108,7 @@ class RowlessModel(object):
             self.out_3_real = self.create_complex_embeddings(self.out_3, "real")
             self.out_3_im = self.create_complex_embeddings(self.out_3, "complex")
 
-    # Creating final outputs that will go in the loss function
+    # Creating final outputs that will go in the loss function in real embeddings case
     def create_outputs_for_real_loss(self):
         self.out_1 = self.out_lstm_1
         self.out_2 = tf.where(self.use_LSTM_2, self.out_lstm_2, self.out_r2)
@@ -132,18 +126,6 @@ class RowlessModel(object):
             r1_r3 = tf.reduce_sum(self.out_1_real * self.out_3_real, axis=1) + \
                         tf.reduce_sum(self.out_1_im * self.out_3_im, axis=1)
             self.loss = tf.reduce_mean(-tf.log(tf.sigmoid(r1_r2 - r1_r3)))
-
-
-            # self.loss_sentence = tf.reduce_mean(-tf.log(
-            #     tf.sigmoid(tf.reduce_sum(tf.multiply(self.out_lstm_1, self.out_lstm_2), axis=1, keep_dims=True) -
-            #                tf.reduce_sum(tf.multiply(self.out_lstm_1, self.out_lstm_3), axis=1, keep_dims=True))))
-            # if self.kb_relation_use:
-            #     self.loss_relation_1 = tf.reduce_mean(-tf.log(tf.sigmoid(
-            #         tf.reduce_sum(tf.multiply(self.out_lstm_1, self.out_r2), axis=1, keep_dims=True) -
-            #         tf.reduce_sum(tf.multiply(self.out_lstm_1, self.out_lstm_3), axis=1, keep_dims=True))))
-            #     self.loss_relation_2 = tf.reduce_mean(-tf.log(tf.sigmoid(
-            #         tf.reduce_sum(tf.multiply(self.out_lstm_1, self.out_lstm_2), axis=1, keep_dims=True) -
-            #         tf.reduce_sum(tf.multiply(self.out_lstm_1, self.out_r3), axis=1, keep_dims=True))))
 
     def training(self):
         global_step_1 = tf.Variable(0, trainable=False, name='global_step_1')
