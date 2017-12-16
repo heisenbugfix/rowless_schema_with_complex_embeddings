b6d541094f214432473dc96a9b1e5186131b03ec
Mili Shah <13bce105@nirmauni.ac.in>
2017-12-16 14:43:17 -0500

    minor changes to train_embeddings

--- code/train_embeddings.py
+++ code/train_embeddings.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import re
 import gc
 
 org_data_path = '/iesl/canvas/aranjan/rowless/all_data/kb_eps_only/'
@@ -8,33 +7,9 @@ data_path = '/iesl/canvas/aranjan/rowless/'
 
 embeddings_size = 25
 
-def make_text_transcript(sents_files, max_sentence_size=0, text_transcript_overwrite=True):
-    if text_transcript_overwrite:
-        with open(data_path+'temp/text_transcript.txt','wt') as f:
-            f.write('')
-    for sent_file in sents_files:
-        try:
-            with open(sent_file,'rb') as f:
-                test_lines = [i.decode().strip().split('\t') for i in f.readlines()]
-            test_lines = np.array([i[-1] for i in test_lines if len(i)==13])
-            max_sentence_size = max([len(i.split(' ')) for i in test_lines]+[max_sentence_size])
-            text = ' '.join(test_lines)
-            text = text.encode('utf-8')
-            with open(data_path+'temp/text_transcript.txt','at') as f:
-                f.write(text+' ')
-        except FileNotFoundError:
-            print(sent_file,'does not exist')
-        gc.collect()
-        print(sent_file,'processed')
-    return max_sentence_size
-
-files = [(org_data_path+'kb_0'+str(i)) if i<10 else (org_data_path+'kb_'+str(i)) for i in range(20)]
-max_sent_size = make_text_transcript(files)
-
-with open(data_path+'temp/max_sent_size.txt','wt') as f:
-    f.write(str(max_sent_size))
+gc.collect()
 
 print('Starting training of embeddings..')
 import fasttext
-embeddings_model = fasttext.skipgram(data_path+'temp/text_transcript.txt',data_path+'temp/embeddings_model_2',min_count=1,dim=embeddings_size)
+embeddings_model = fasttext.skipgram(data_path+'temp/text_transcript.txt',data_path+'temp/embeddings_model',min_count=1,dim=embeddings_size)
 print('completed training of embeddings..')
