a2773d7605e8f5c49e2186bcc1bf745ba7ab2953
ashish <ashish.ranjan2000@gmail.com>
2017-12-17 15:01:47 -0500

    added word vec embeddings before LSTM layer

--- code/model.py
+++ code/model.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 # TODO: Make model more generic. Need to abstractions
 class RowlessModel(object):
     # Create input placeholder for the network
-    def __init__(self, wordvecdim, num_units, num_relations=None, embedding_size=None, emb_type="real"):
+    def __init__(self, vocab_size, wordvecdim, num_units, num_relations, embedding_size, emb_type="real"):
 
         assert embedding_size == num_units
         assert num_relations is not None
@@ -15,26 +15,35 @@ class RowlessModel(object):
             self.embedding_size = embedding_size
 
         self.emb_type = emb_type
+        #KB
         self.create_conditional_placeholders()
         self.create_placeholders_kb()
         self.create_kb_outputs()
+        # WORD_EMB
+        self.vocab_size = vocab_size
         self.wordvec_dim = wordvecdim
+        self.create_placeholders_word_emb()
+        self.create_wemb_outputs()
+        #LSTM
         self.num_units = num_units
         self.create_placeholders_lstm()
         self.create_lstm_outputs()
+        #OUTPUTS
         self.create_outputs_for_real_loss()
         # Add layer for complex embedding
         if self.emb_type == "complex":
             self.create_outputs_for_complex_loss()
+        #LOSS, TRAIN, INITIALIZE
         self.loss()
         self.training()
         self.sess = tf.Session()
+        self.init_for_word_emb()
         self.init = tf.global_variables_initializer()
         self.sess.run(self.init)
         for v in tf.global_variables():
             print(v.name)
 
-    # Create KB embeddings
+    # Creates KB embeddings
     def create_kb_embeddings(self, rel_ids):
         self.rel_embeddings = tf.get_variable("relation_embeddings",
                                               [self.num_relations, self.embedding_size], dtype=tf.float32)
@@ -42,19 +51,19 @@ class RowlessModel(object):
         # emb_rel_ids = tf.reshape(emb_rel_ids, shape=[-1, emb_rel_ids.shape[2]])
         return emb_rel_ids
 
-    # Creating conditional placeholders for switching inputs between LSTM and KB Embeddings
+    # Creates conditional placeholders for switching inputs between LSTM and KB Embeddings
     def create_conditional_placeholders(self):
         self.use_LSTM_2 = tf.placeholder(tf.bool, shape=[None,])
         self.use_LSTM_3 = tf.placeholder(tf.bool, shape=[None,])
 
-    # generate the outputs for each input
+    # generates the outputs for each input
     def create_lstm_outputs(self):
         with tf.variable_scope('shared_lstm') as scope:
-            self.out_lstm_1 = self.lstm_share(self.num_units, self.input_LSTM_1, self.seq_len_LSTM_1)
+            self.out_lstm_1 = self.lstm_share(self.num_units, self.out_s1_wv, self.seq_len_LSTM_1)
             scope.reuse_variables()  # the variables will be reused.
-            self.out_lstm_2 = self.lstm_share(self.num_units, self.input_LSTM_2, self.seq_len_LSTM_2)
+            self.out_lstm_2 = self.lstm_share(self.num_units, self.out_s2_wv, self.seq_len_LSTM_2)
             scope.reuse_variables()
-            self.out_lstm_3 = self.lstm_share(self.num_units, self.input_LSTM_3, self.seq_len_LSTM_3)
+            self.out_lstm_3 = self.lstm_share(self.num_units, self.out_s3_wv, self.seq_len_LSTM_3)
 
     def create_kb_outputs(self):
         with tf.variable_scope('shared_kb') as scope:
@@ -62,14 +71,41 @@ class RowlessModel(object):
             scope.reuse_variables()  # the variables will be reused.
             self.out_r3 = self.create_kb_embeddings(self.input_3_r3)
 
+    # creates placeholders for word_emb. This will be input to LSTM
+    def create_placeholders_word_emb(self):
+        self.input_1_s1 = tf.placeholder(tf.int32, [None, None], name="wvec_s1")
+        self.input_2_s2 = tf.placeholder(tf.int32, [None, None], name="wvec_s2")
+        self.input_3_s3 = tf.placeholder(tf.int32, [None, None], name="wvec_s3")
+        self.word_emb_placeholder = tf.placeholder(tf.float32, [self.vocab_size, self.wordvec_dim])
+
+    # Initializer for word_embedding
+    def init_for_word_emb(self):
+        self.word_emb_init = self.word_embeddings.assign(self.word_emb_placeholder)
+
+    # Creates KB embeddings
+    def create_word_embeddings(self, wv_ids):
+        self.word_embeddings = tf.get_variable("WordVec_Embedding", [self.vocab_size, self.wordvec_dim], dtype=tf.float32,
+                                           trainable=False)
+        wemb_ids = tf.nn.embedding_lookup(self.word_embeddings, wv_ids)
+        return wemb_ids
+
+    def create_wemb_outputs(self):
+        with tf.variable_scope('shared_wemb') as scope:
+            self.out_s1_wv = self.create_word_embeddings(self.input_1_s1)
+            scope.reuse_variables()  # the variables will be reused.
+            self.out_s2_wv = self.create_word_embeddings(self.input_2_s2)
+            scope.reuse_variables()  # the variables will be reused.
+            self.out_s3_wv = self.create_word_embeddings(self.input_3_s3)
+
+
     # Placeholders for input data to LSTM
     def create_placeholders_lstm(self):
         # shape (batch_size, timesteps, wordvec_dim)
-        self.input_LSTM_1 = tf.placeholder(tf.float32, [None, None, self.wordvec_dim], name="s1")
+        # self.input_LSTM_1 = tf.placeholder(tf.float32, [None, None, self.wordvec_dim], name="s1")
         self.seq_len_LSTM_1 = tf.placeholder(tf.uint8, [None,])
-        self.input_LSTM_2 = tf.placeholder(tf.float32, [None, None, self.wordvec_dim], name="s2")
+        # self.input_LSTM_2 = tf.placeholder(tf.float32, [None, None, self.wordvec_dim], name="s2")
         self.seq_len_LSTM_2 = tf.placeholder(tf.uint8, [None,])
-        self.input_LSTM_3 = tf.placeholder(tf.float32, [None, None, self.wordvec_dim], name="s3")
+        # self.input_LSTM_3 = tf.placeholder(tf.float32, [None, None, self.wordvec_dim], name="s3")
         self.seq_len_LSTM_3 = tf.placeholder(tf.uint8, [None,])
 
     # Placeholders for relation as input data
