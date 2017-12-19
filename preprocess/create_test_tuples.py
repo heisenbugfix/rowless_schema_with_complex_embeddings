daef49191ca56eb203184cdd3d35e2bd41b0e80e
ashish <ashish.ranjan2000@gmail.com>
2017-12-18 23:15:02 -0500

    preprocessing changes

--- preprocess/create_test_tuples.py
+++ preprocess/create_test_tuples.py
@@ -1,21 +1,24 @@
+import sys
+sys.path.insert(0,"./preprocess")
 import numpy as np
 import pandas as pd
 import re
 import gc
 import pickle
+import json
 
 from create_test_tuples_util import *
 
 # org_data_path = '/iesl/canvas/aranjan/rowless/all_data/kb_eps_only/'
 # data_path = '/iesl/canvas/aranjan/rowless/'
-org_data_path = "../data/"
-data_path = "../data/"
+org_data_path = "../data/rowless_data/"
+data_path = "../data/rowless_data/"
 
 embeddings_size = 50
 max_sent_size = 35
 test_neg_rel_size = 10
 
-with open(data_path+'temp/word_to_id_map.pickle','rb') as f:
+with open(data_path+'word_to_id_map.pickle','rb') as f:
     word_to_id_map = pickle.load(f)
 embeddings_model = word_to_id_map
 print('Embeddings loaded...')
@@ -26,16 +29,16 @@ print('Starting preprocessing...')
 ids,emb,seq_lens = preprocess_file(org_data_path+'kb_test_35_cap10',embeddings_model,max_sent_size)
 print(ids.shape,emb.shape,seq_lens.shape)
 
-with open(org_data_path+'intermediate/test_ids.pickle','wb') as f:
+with open(org_data_path+'test_ids.pickle','wb') as f:
     pickle.dump(ids,f,protocol=2)
 try:
-	with open(org_data_path+'intermediate/test_emb.pickle','wb') as f:
+	with open(org_data_path+'test_emb.pickle','wb') as f:
 	    pickle.dump(emb,f,protocol=2)
 except:
-	with open(org_data_path+'intermediate/test_emb.json','w') as f:
+	with open(org_data_path+'test_emb.json','w') as f:
 		print('JSONing emb')
 		json.dump(emb.tolist(),f)
-with open(org_data_path+'intermediate/test_se_lens.pickle','wb') as f:
+with open(org_data_path+'test_se_lens.pickle','wb') as f:
     pickle.dump(seq_lens,f,protocol=2)
 print('Entity pairs, Sentence embeddings, Sequence lengths pickled')
 
@@ -50,25 +53,30 @@ print('Entity pairs, Sentence embeddings, Sequence lengths loaded')
 """
 
 
-with open(org_data_path+'temp/relations_dict.pickle','rb') as f:
+with open(org_data_path+'relations_dict.pickle','rb') as f:
     relations_dict = pickle.load(f)
 print('Read relations dict')
 
 
-with open(org_data_path+'test.txt') as f:
+# with open('../data/test.txt') as f:
+with open('../data/allrelations.txt') as f:
     relations = np.array([i.split('\t') for i in f.readlines()],dtype=np.object)
 relations[:,2] = np.array([int(relations_dict[i]) for i in relations[:,2]])
 
 
 ent_pair_idx_sent = create_entity_pairs_index_sentences(ids)
-ent_pair_idx_rel = create_entity_pairs_index_relations(relations[:[0,1]])
+ent_pair_idx_rel = create_entity_pairs_index_relations(relations[:,[0,1]])
 
+print(len(ent_pair_idx_sent))
+print(len(ent_pair_idx_rel))
 
 tuples = create_test_tuples(ent_pair_idx_sent, ent_pair_idx_rel, emb, seq_lens, relations[:,2], test_neg_rel_size)
 try:
-	with open(data_path+'temp/preprocessed_test.pickle','wb') as f:
+	with open(data_path+'preprocessed_test.pickle','wb') as f:
 		pickle.dump(tuples,f,protocol=2)
 except:
-	with open(data_path+'temp/preprocessed_test.json','w') as f:
+	with open(data_path+'preprocessed_test.json','w') as f:
 		print('JSONing results')
 		json.dump([i.tolist() for i in tuples],f)
+
+print(len(tuples))
--- preprocess/create_test_tuples_util.py
+++ preprocess/create_test_tuples_util.py
@@ -7,11 +7,14 @@ import pickle
 global cnt
 cnt = 0
 
+
 def get_e1_embedding_id():
     return 1
 
+
 def get_e2_embedding_id():
     return 2
+
 
 def preprocess_file(f, embeddings_model, max_sent_size):
     """
@@ -20,43 +23,36 @@ def preprocess_file(f, embeddings_model, max_sent_size):
         np.array [n_sents x timesteps] => <sent_embeddings>
         np.array [n_sents x 1] => <seq_lengths>
     """
-    global cnt
     def create_sent_embeddings(preprocessed_sent):
-        global cnt
-        sent = [str(i) for i in preprocessed_sent[sent_idx].split(' ')]
+        #TODO: limit to_ret to max_sent_size, put check (max_sent_size-len(to_ret)) in return
+        sent = preprocessed_sent[sent_idx].split(' ')
         e1_s = preprocessed_sent[e1_start_idx]
         e1_e = preprocessed_sent[e1_end_idx]
         e2_s = preprocessed_sent[e2_start_idx]
         e2_e = preprocessed_sent[e2_end_idx]
-        if(len(sent)>max_sent_size):
+        if(len(sent)>max_sent_size) or e1_e>max_sent_size or e2_e>max_sent_size:
+            return []
             raise ValueError('Sentence size greater than maximum allowed')
-        try:
-	        if e1_e <= e2_s:
-	            to_ret = [embeddings_model[i] for i in sent[:e1_s]]\
-	                            + [get_e1_embedding_id()]\
-	                + [embeddings_model[i] for i in sent[e1_e:e2_s]]\
-	                   + [get_e2_embedding_id()]\
-	                + [embeddings_model[i] for i in sent[e2_e:]]
-	        elif e2_e <= e1_s:
-	            to_ret = [embeddings_model[i] for i in sent[:e2_s]]\
-	                            + [get_e2_embedding_id()]\
-	                + [embeddings_model[i] for i in sent[e2_e:e1_s]]\
-	                   + [get_e1_embedding_id()]\
-	                + [embeddings_model[i] for i in sent[e1_e:]]
-	        else:
-	            raise ValueError('Entity 1 and Entity 2 indexes are incorrect')
-        except NameError as e:
-        	cnt += 1
-        	# if 'gypsy' in e:
-        		# raise ValueError
-        	print(sent)
-        	return []
-        except KeyError as e:
-        	cnt += 1
-        	print(sent)
-        	return []
-        return to_ret + [0 for i in range(max_sent_size-len(to_ret))]
-    
+        if e1_e <= e2_s:
+            to_ret = [embeddings_model[i] for i in sent[:e1_s]]\
+                            + [get_e1_embedding_id()]\
+                + [embeddings_model[i] for i in sent[e1_e:e2_s]]\
+                   + [get_e2_embedding_id()]\
+                + [embeddings_model[i] for i in sent[e2_e:]]
+        elif e2_e <= e1_s:
+            to_ret = [embeddings_model[i] for i in sent[:e2_s]]\
+                            + [get_e2_embedding_id()]\
+                + [embeddings_model[i] for i in sent[e2_e:e1_s]]\
+                   + [get_e1_embedding_id()]\
+                + [embeddings_model[i] for i in sent[e1_e:]]
+        else:
+            raise ValueError('Entity 1 and Entity 2 indexes are incorrect')
+        to_ret = to_ret + [0 for i in range(max_sent_size-len(to_ret))]
+        if len(to_ret)==36:
+            print(preprocessed_sent)
+            raise ValueError
+        return to_ret
+
     """
     Columns of test_lines:
         0 e1
@@ -77,43 +73,45 @@ def preprocess_file(f, embeddings_model, max_sent_size):
         test_lines[:,i] = test_lines[:,i].astype(np.int)
     test_lines = test_lines[:,[0,2,3,4,5,7,8,9,12]]
     print('Test lines made...')
-    
+
     sent_idx = 8
     e1_start_idx = 2
     e1_end_idx = 3
     e2_start_idx = 6
     e2_end_idx = 7
-    
     emb = []
     seq_lens = []
     ep = []
     for i in test_lines:
-    	e = create_sent_embeddings(i)
-    	if len(e)!=0:
-    		emb.append(e)
-    		seq_lens.append(len(i[8].split(' ')))
-    		ep.append(i[[0,4]])
+        sent_emb = create_sent_embeddings(i)
+        if len(sent_emb)==0:
+            continue
+        emb.append(sent_emb)
+        seq_lens.append(len(i[8].split(' ')))
+        ep.append(i[[0,4]])
+
     emb = np.array(emb,dtype=np.int32)
     seq_lens = np.array(seq_lens,dtype=np.uint8)
     ep = np.array(ep)
-
     print('Embeddings and IDs made...')
-    return ep,emb,seq_lens
+    return ep, emb, seq_lens
 
 def create_entity_pairs_index_sentences(entity_pairs_sents):
     """
     Creates a dict for indexes of all the sentences with the same entity pairs.
     Key : <e1,e2>
     Values : np.ndarray([s1, s2, ...])
-    """ 
+    """
     d = dict()
-    for idx,ep in enumerate(entity_pairs_sents):
-        key = (ep[0],ep[1])
+    for idx, ep in enumerate(entity_pairs_sents):
+        key = (ep[0], ep[1])
         if key in d:
-            d[key] = np.append(d[key],idx)
+            d[key] = np.append(d[key], idx)
         else:
-            d[key] = np.array([idx],dtype=np.int32)
-    
+            d[key] = np.array([idx], dtype=np.int32)
+    return d
+
+
 def create_entity_pairs_index_relations(entity_pairs_rels):
     """
     Creates a dict for indexes of all the sentences with the same entity pairs.
@@ -121,41 +119,50 @@ def create_entity_pairs_index_relations(entity_pairs_rels):
     Values : np.ndarray([r1, r2, ...])
     """
     d = dict()
-    for idx,ep in enumerate(entity_pairs_rels):
-        key = (ep[0],ep[1])
+    for idx, ep in enumerate(entity_pairs_rels):
+        key = (ep[0], ep[1])
         if key in d:
-            d[key] = np.append(d[key],idx)
+            d[key] = np.append(d[key], idx)
         else:
-            d[key] = np.array([idx],dtype=np.int32)
+            d[key] = np.array([idx], dtype=np.int32)
     return d
 
+
 def create_test_tuples(ent_pair_idx_sent, ent_pair_idx_rel, sents_embeddings, seq_lens, relations, test_neg_rel_size):
-	tuples = []
-	disp_step = -1
+    tuples = []
+    disp_step = -1
 
-	all_idxs_rels = np.arange(len(relations))
+    all_idxs_rels = np.arange(len(relations))
 
-	for ep in ent_pair_idx_sent.keys():
-		disp_step += 1
-		if disp_step%10==0:
-			print('On entity pair',disp_step)
+    for ep in ent_pair_idx_sent.keys():
+        disp_step += 1
+        if disp_step % 1000 == 0:
+            print('On entity pair', disp_step)
+            print(len(tuples))
 
-		if (not ent_pair_idx_sent[ep]) or len(ent_pair_idx_sent[ep])==0\
-		 or (not ent_pair_idx_rel[ep]) or len(ent_pair_idx_rel[ep])==0:
-			continue
+        if ep in ent_pair_idx_rel:
+            sents = sents_embeddings[ent_pair_idx_sent[ep]]
+            seqs = seq_lens[ent_pair_idx_sent[ep]]
+            pos_rels = relations[ent_pair_idx_rel[ep]]
+            neg_rels = []
+            for pos_rel in pos_rels:
+                neg_idx = [i for i in all_idxs_rels if i not in ent_pair_idx_rel[ep]]
+                neg_idx = np.random.choice(neg_idx, test_neg_rel_size)
+                neg_block = relations[neg_idx]
+                neg_rels.append(neg_block)
+        elif (ep[1],ep[0]) in ent_pair_idx_rel:
+            sents = sents_embeddings[ent_pair_idx_sent[ep]]
+            seqs = seq_lens[ent_pair_idx_sent[ep]]
+            pos_rels = relations[ent_pair_idx_rel[(ep[1],ep[0])]]
+            neg_rels = []
+            for pos_rel in pos_rels:
+                neg_idx = [i for i in all_idxs_rels if i not in ent_pair_idx_rel[ (ep[1],ep[0]) ]]
+                neg_idx = np.random.choice(neg_idx, test_neg_rel_size)
+                neg_block = relations[neg_idx]
+                neg_rels.append(neg_block)
+        else:
+            continue
 
-		sents = sents_embeddings[ent_pair_idx_sent[ep]]
-		seqs = seq_lens[ent_pair_idx_sent[ep]]
+        tuples.append([sents, seqs, pos_rels, neg_rels])
 
-		pos_rels = relations[ent_pair_idx_rel[ep]]
-
-		neg_rels = []
-		for pos_rel in pos_rels:
-			neg_idx = [i for i in all_idxs_rels if i not in ent_pair_idx_rel[ep]]
-			neg_idx = np.random.choice(neg_idx,test_neg_rel_size)
-			neg_block = relations[neg_idx]
-			neg_rels.append(neg_block)
-
-		tuples.append([sents,seqs,pos_rels,neg_rels])
-
-	return tuples
+    return tuples
